72aa18fda4b48b908acc05837cae17179e10cde0
shaowei <johnny960729839@gmail.com>
2025-08-19 17:06:49 +0700

    Update airports.js

--- api/airports.js
+++ api/airports.js
@@ -1,3 +1,17 @@
-export default async function handler(req, res) {
-  res.status(200).json({ ok: true, msg: "airports api alive" });
+export const config = { runtime: "edge" };
+
+export default async function handler(req) {
+  const data = [
+    { code: "BKK", name: "曼谷" },
+    { code: "CNX", name: "清迈" },
+    { code: "PEK", name: "北京首都" },
+    { code: "PKX", name: "北京大兴" },
+    { code: "PVG", name: "上海浦东" },
+    { code: "SHA", name: "上海虹桥" },
+    { code: "CAN", name: "广州" },
+  ];
+  return new Response(JSON.stringify({ ok: true, data }), {
+    headers: { "content-type": "application/json; charset=utf-8" },
+    status: 200,
+  });
 }
