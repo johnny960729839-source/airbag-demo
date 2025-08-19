8682426b73075388d6d452e662da409a9790eebf
shaowei <johnny960729839@gmail.com>
2025-08-19 20:52:13 +0700

    Update airports.js

--- api/airports.js
+++ api/airports.js
@@ -1,17 +1,11 @@
-export const config = { runtime: "edge" };
+// /api/airports.js
+const LIST = [
+  { code: "BKK", name: "曼谷" }, { code: "CNX", name: "清迈" },
+  { code: "CAN", name: "广州" }, { code: "PVG", name: "上海浦东" },
+  { code: "SHA", name: "上海虹桥" }, { code: "PEK", name: "北京首都" },
+  { code: "PKX", name: "北京大兴" }
+];
 
-export default async function handler(req) {
-  const data = [
-    { code: "BKK", name: "曼谷" },
-    { code: "CNX", name: "清迈" },
-    { code: "PEK", name: "北京首都" },
-    { code: "PKX", name: "北京大兴" },
-    { code: "PVG", name: "上海浦东" },
-    { code: "SHA", name: "上海虹桥" },
-    { code: "CAN", name: "广州" },
-  ];
-  return new Response(JSON.stringify({ ok: true, data }), {
-    headers: { "content-type": "application/json; charset=utf-8" },
-    status: 200,
-  });
+export default function handler(req, res) {
+  res.status(200).json({ ok: true, data: LIST });
 }
